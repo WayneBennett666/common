73be77ebd37234561bab580fb792f5a843d7b3c0
beatzaplenty <wayne.bennett@live.com>
2023-12-27 18:31:24 +1000

    tweaked remote update command 	modified:   src/my_functions_beatzaplenty/general_purpose.py

--- src/my_functions_beatzaplenty/general_purpose.py
+++ src/my_functions_beatzaplenty/general_purpose.py
@@ -141,7 +141,7 @@ def remote_update(config,keyfile,containers):
     else:
         ssh_port =  config.get("ssh_port") 
     if config.get('update_script') is None:
-        update_script = f".local/lib/python3.10/site-packages/my_functions_beatzaplenty/remote_update.py {containers}"
+        update_script = f"-m my_functions_beatzaplenty.remote_update {containers}"
     else:
         update_script = config.get('update_script')
     
