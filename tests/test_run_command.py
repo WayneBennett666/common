a99e3aa6d014e8ac52292a6df3bc56b969322d79
beatzaplenty <wayne.bennett@live.com>
2023-12-28 20:34:52 +1000

    started work on unit testing 	new file:   .gitignore 	new file:   .vscode/settings.json 	new file:   tests/__init__.py 	new file:   tests/test_run_command.py 	modified:   src/my_functions_beatzaplenty/general_purpose.py

---
+++ tests/test_run_command.py
@@ -0,0 +1,22 @@
+import pytest, subprocess
+#     ###################### LOAD TESTING MODULE  ###########################
+from importlib.machinery import SourceFileLoader
+test = SourceFileLoader("run_command", "/home/wayne/common/src/my_functions_beatzaplenty/general_purpose.py").load_module()
+  #############################################################################
+
+def test_run_command_successful_execution():
+    command = ["echo", "Hello, World!"]
+    assert test.run_command(command) is True
+
+def test_run_command_failure():
+    command = ["nonexistent_command"]
+    assert test.run_command(command) is False
+
+def test_run_command_exception_handling():
+    command = ["command_with_syntax_error"]
+    # Replace the command above with one that would cause an exception
+    # in your run_command function, e.g., by having invalid syntax.
+    with pytest.raises(Exception):
+        test.run_command(subprocess.CalledProcessError)
+
+# You can add more tests based on the specific scenarios you want to cover.
