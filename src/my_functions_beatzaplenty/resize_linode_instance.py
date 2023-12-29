7059ef93ba44822ee49f50a5aa64ecc27d29191c
beatzaplenty <wayne.bennett@live.com>
2023-12-29 22:46:21 +1000

    Build ver 2.24. Fix syntax error on api_key 	modified:   pyproject.toml 	modified:   src/my_functions_beatzaplenty/resize_linode_instance.py

--- src/my_functions_beatzaplenty/resize_linode_instance.py
+++ src/my_functions_beatzaplenty/resize_linode_instance.py
@@ -20,7 +20,7 @@ def main(config=None,arg_direction=None,arg_monitor=False):
 
     ################## Linode Data Aquisition ###########################
     try:
-        api_client = linode_api.LinodeClient(os.getenv('LINODE_API_KEY'))
+        api_client = linode_api.LinodeClient(os.environ['LINODE_API_KEY'])
         linodes = api_client.linode.instances(linode_api.Instance.label == config.get('linode_name'))
     except linode_api.errors.ApiError as e:
         print(f"Error during Linode API call: {e}")
